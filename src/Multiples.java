bcd99569b1a904d5013c61e258603feb70ae02e2
Geon Lim <imgeon@imgeonucbookpro.home>
2024-09-10 11:53:47 -0400

    new file for the task4

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0) {
+                sum += 1;
+            }
+            else if (i % 5 == 0) {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
